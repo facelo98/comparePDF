42b601dbd3b6e0ccb9b6f295f3942c1257870150
Fabio Celotto <fabio_209@outlook.it>
2025-07-13 19:41:20 +0200

    Requirements update and setup bugfix

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 from cx_Freeze import setup, Executable
 
 build_exe_options = {
+    "packages": ["comtypes"],
+    "includes": ["comtypes.stream"],
     "include_files": [
-        ("source", "source")
+        ("source", "source"),
+        ("src", "src"),
+        ("C:/Users/Fabio/Documents/comparePDF/lib/site-packages/comtypes/stream.py", "lib/comtypes/stream.py")
+
     ]
 }
 
 setup(
     name="comparePDF",
-    version="1.1.1",
+    version="1.1.2",
     options={"build_exe": build_exe_options},
     executables=[Executable("main.py")]
 )
